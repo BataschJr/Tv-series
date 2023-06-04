2efd86931c2473638079c70cb14771d2f8e46d3f
BataschJr <adnanworks24@gmail.com>
2023-06-05 01:04:42 +0530

    Added api which fetches the details of the season and episodes

--- src/components/singleSeries.js
+++ src/components/singleSeries.js
@@ -1,18 +1,30 @@
 import { useParams } from 'react-router';
 
-import Loader from './loader';
 import { useEffect, useState } from 'react';
 
 function SingleSeries(props) {
-  const [seriesState, setSeriesState] = useState(null);
-  let { id } = useParams();
+  const [seriesName, setSeriesName] = useState(null);
+  const { id } = useParams();
 
-  useEffect(() =>
-    fetch(`https://api.tvmaze.com/alternatelists/${id}?embed=alternateepisodes`)
+  useEffect(() => {
+    fetch(`https://api.tvmaze.com/shows/${id}?embed=cast`)
       .then((response) => response.json())
-      .then((json) => setSeriesState(json))
+      .then((json) => setSeriesName((seriesName) => json));
+  }, []);
+  return (
+    <div>
+      {seriesName === null && <p>Loading...</p>}
+      {seriesName !== null && (
+        <div>
+          <p>{seriesName.name}</p>
+          <p>Premiered - {seriesName.premiered}</p>
+          <p>Rating - {seriesName.rating.average}</p>
+          <p>Genre - {seriesName.genres}</p>
+          {seriesName.image === null ? <p>No image has been found</p> : <img src={seriesName.image.medium} alt="Show image" />}
+        </div>
+      )}
+    </div>
   );
-  return <p>Series - {id}</p>;
 }
 
 export default SingleSeries;
